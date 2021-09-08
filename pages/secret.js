5f1ef26f69e1eb7665cc105254e0c3ee5c8b094b
derrekcoleman <80121818+derrekcoleman@users.noreply.github.com>
2021-09-08 13:19:48 -0700

    Made 'secret' simpler for testing

--- pages/secret.js
+++ pages/secret.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import router, { useRouter } from 'next/router';
 
-export default function Secret() {
-  //if (true) {router.push('/gtfo')}
-
+function Secret() {
   return (
     <div>
       <h1>Welcome to the secret club!</h1>
     </div>
   );
 }
+
+export default Secret;
